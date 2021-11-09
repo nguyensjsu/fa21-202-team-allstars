be327a6dad2f4ec011dbbeda435a71c5c53ad26a
Mohamed Shafeeq Usman <mohamedshafeeq.usman@sjsu.edu>
2021-11-08 19:36:03 -0800

    Bullet Collision Strategy Classes

---
+++ tanks/Health0Strategy.java
@@ -0,0 +1,16 @@
+/**
+ * Write a description of class Health0Strategy here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Health0Strategy implements IBulletCollisionStrategy
+{
+    /**
+     * Display the tank destroyed (after all the lives are lost)
+     */
+    public void display(){
+        
+    }
+    
+}
---
+++ tanks/Health1Strategy.java
@@ -0,0 +1,16 @@
+/**
+ * Write a description of class Health1Strategy here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Health1Strategy implements IBulletCollisionStrategy
+{   
+    /** 
+     * Display the tank (not destroyed)
+     */
+    public void display(){
+        
+    }
+    
+}
